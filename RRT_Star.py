fcbf2c65bfddfc665e80dd9b9b9b247b65edc07e
Gabriel Entov <gentov97@gmail.com>
2020-02-20 00:11:29 -0500

    implemented nodes along connected line, weird bug with closest connection not happening sometimes

--- RRT_Star.py
+++ RRT_Star.py
@@ -15,9 +15,9 @@ class RRT_Star():
         self.maxIterations = 5000
         self.foundGoal = False
         self.nodes = []
-        self.expandDistance = 10
+        self.expandDistance = 20
         self.win = Tk()
-        self.speed = 80
+        self.speed = 100
         self.win.geometry('700x800')
         self.win.configure(background="grey")
         self.win.title("Test")
@@ -44,6 +44,7 @@ class RRT_Star():
             if(distance < smallestDistance):
                 nearestNode = self.nodes[i]
                 smallestDistance = distance
+                print("Smallest Distance: ", smallestDistance)
 
         return nearestNode
 
@@ -56,16 +57,67 @@ class RRT_Star():
     def drawPoint(self, x, y, color):
         self.canvas.create_oval(x - 5,y - 5,x+5,y+5, fill = color)
 
-    def connect(self, newNode, parent, color = None):
-        if(color is None):
-            self.canvas.create_line(newNode.x, newNode.y, parent.x, parent.y)
-        else:
-            self.canvas.create_line(newNode.x, newNode.y, parent.x, parent.y, width = 8, fill = color)
+    def connect(self, randomNode, nearestNode, color = None):
+        # instead of connecting directly, every 5 pixels, place a new node along the line, and check for obstacle
+        # find vector put points on it
+        parent = nearestNode
+        newNode = randomNode
+        distance = 10
+        # if we are really close from the start, no need to go on.
+        if (self.calculateDistance(newNode, parent) < distance):
+            self.drawPoint(newNode.x, newNode.y, color='blue')
+            # connect parent and new node
+            self.canvas.create_line(parent.x, parent.y, newNode.x, newNode.y)
+            self.drawPoint(newNode.x, newNode.y, color='blue')
+            newNode.parent = parent
+            self.nodes.append(newNode)
+            return
+
+        yDiff = newNode.y - parent.y
+        xDiff = newNode.x - parent.x
+        angle = math.atan2(yDiff, xDiff)
+        xIncrement = distance*math.cos(angle)
+        yIncrement = distance*math.sin(angle)
+        newNodeInLine = Node(x = parent.x + xIncrement, y = parent.y + yIncrement)
+        newNodeInLine.parent = parent
+
+        while(self.calculateDistance(newNodeInLine, newNode) > distance):
+            # create a node
+            if(not self.checkCollision(newNodeInLine)):
+                # append it to the node list
+                self.nodes.append(newNodeInLine)
+                #draw the node
+                self.drawPoint(newNodeInLine.x, newNodeInLine.y, color = 'blue')
+                # draw a line between the previous one and the new one
+                self.canvas.create_line(newNodeInLine.x, newNodeInLine.y, parent.x, parent.y)
+                # the new node becomes the parent
+                parent = newNodeInLine
+                # make a new node on the line
+                newNodeInLine = Node(x = parent.x + xIncrement, y = parent.y + yIncrement)
+                # set the parent of the new node to the previous one on the line
+                newNodeInLine.parent = parent
+            else:
+                break
+        #if we are close to the original new node
+        if(self.calculateDistance(newNodeInLine, newNode) < distance):
+            self.drawPoint(newNodeInLine.x, newNodeInLine.y, color='blue')
+            # draw a line between the last one on the line and the new one on the line
+            self.canvas.create_line(newNodeInLine.parent.x, newNodeInLine.parent.y, newNodeInLine.x, newNodeInLine.y)
+            self.drawPoint(newNode.x, newNode.y, color='blue')
+            newNode.parent = newNodeInLine
+            # draw a line between the last one in the line and the original point we made
+            self.canvas.create_line(newNodeInLine.x, newNodeInLine.y, newNode.x, newNode.y)
+            #append it to the list of nodes we have
+            self.nodes.append(newNode)
+
+    def directConnect(self, newNode, parent, color = None):
+        #Directly draw a line between two nodes, do not put nodes in between them
+        self.canvas.create_line(newNode.x, newNode.y, parent.x, parent.y, fill = color, width = 10)
 
     def drawFinalPath(self):
         node = endNode
         while(node.parent is not None):
-            self.connect(node, node.parent, color = 'green')
+            self.directConnect(node, node.parent, color = 'green')
             time.sleep(1/self.speed)
             node = node.parent
             self.win.update()
@@ -93,14 +145,10 @@ class RRT_Star():
             #find closest node in list and return it
             nearestNode = self.getNearestNode(randomNode)
             print("Nearest Node to New Node at Coordinates:", nearestNode.x, nearestNode.y)
-            randomNode.parent = nearestNode
-            print("Parent to new node set")
+            #I think the following line is taken care of in connect
+            # randomNode.parent = nearestNode
+            # print("Parent to new node set")
             self.connect(randomNode, nearestNode)
-
-            if(self.checkCollision(randomNode) == 0):
-                print("Added new Node to list")
-                self.nodes.append(randomNode)
-                self.drawPoint(randomNode.x, randomNode.y, 'blue')
 
             distanceToGoal = self.calculateDistance(self.nodes[-1], self.endNode)
             if(distanceToGoal <= self.expandDistance):
